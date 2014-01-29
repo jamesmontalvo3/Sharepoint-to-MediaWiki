1650c90448a3b41ac2fb4ee243bbcaebf506943d
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-01-29 15:41:30 -0600

    fixed 1-get-sp-pages.js

--- 1-get-sp-pages.js
+++ 1-get-sp-pages.js
@@ -11,20 +11,28 @@
 	var s=document.createElement("script");
 	s.src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js";
 	document.body.appendChild(s);
-})();
-
-
-function stealFromSharepointFn() {
-	window.stealFromSharepoint = {};
-
-	$(".itx a").each(function(index,element){ 
-
-		stealFromSharepoint[ $(element).html() ] = "https://modspops.jsc.nasa.gov" + $(element).attr("href").replace(/ /g, "%20");
-
-	});
-
-	window.stolen = JSON.stringify( stealFromSharepoint );
 	
-}
+	var stealFromSharepointFn = function() {
+		console.log( "attempt steal" );
+		window.stealFromSharepoint = {};
 
-setTimeout(stealFromSharepointFn, 1000);
+		jQuery(".itx a").each(function(index,element){ 
+
+			stealFromSharepoint[ jQuery(element).html() ] = "https://modspops.jsc.nasa.gov" + jQuery(element).attr("href").replace(/ /g, "%20");
+
+		});
+
+		window.stolen = JSON.stringify( stealFromSharepoint );
+		
+		// jQuery("#s4-mainarea").prepend(
+			// "<div style='height:500px;'><textarea style='width:90%;height:90%;'>" + window.stolen + "</textarea></div>"
+		// );
+		
+		// jQuery("body").html("<textarea style='width:90%;height:90%;'>" + window.stolen + "</textarea>");
+		jQuery("body").html("<div style='font-family:monospace;'>" + window.stolen + "</div>");
+	};
+	
+	console.log( "Please wait 2 seconds..." );
+	setTimeout(stealFromSharepointFn, 1000);
+	
+})();
