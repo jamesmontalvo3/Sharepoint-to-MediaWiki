aa44c03d503a15df83406b7ff1c3efcb8aad284a
James Montalvo <edwin.j.montalvo@nasa.gov>
2014-01-30 11:57:32 -0600

    fixes to get-page.php, cURL now working

--- lib/get-page.php
+++ lib/get-page.php
@@ -1,13 +1,13 @@
 <?php
+error_reporting( -1 );
+ini_set( 'display_errors', 1 );
 
 $url = $_GET['url'];
 $page = $_GET['page'];
 
-$path_sep = '\\';
 
 echo "<li><a href='$url'>$page</a>";
 
-$cookie_file_path = dirname(__FILE__) . $path_sep . 'cookies.txt';
 $ch = curl_init();
 
 $user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36";
@@ -19,30 +19,31 @@ $user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, li
  *  "Sharepoint Thief" if that makes you happy.
  **/
 require "../usr/cURL_credentials.php";
+$login = $loginDomain . "/" . $loginUser . ":" . $loginPass;
 
+
+// Thanks to: http://www.tunnelsup.com/using-the-sharepoint-2013-wiki-api
 //==========================================================================
-curl_setopt($ch, CURLOPT_USERPWD, $username. ':' . $password);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file_path);
-curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file_path);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-curl_setopt($ch, CURLOPT_FAILONERROR, 0);
-curl_setopt($ch, CURLOPT_MAXREDIRS, 100);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_NTLM);    // Optional only if the sharepoint requires authentication
+curl_setopt($ch, CURLOPT_USERPWD, $login);            // Optional only if the sharepoint requires authentication
+curl_setopt($ch, CURLOPT_USERAGENT,$user_agent);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
 //==========================================================================
-
 
 try {
 	$ret = curl_exec($ch);
 } catch (Exception $e) {
 	die("Curl failed: " . $e->getMessage() );
 }
-file_put_contents("./raw-html/$page.html", $ret, FILE_USE_INCLUDE_PATH); 
+
+if (!file_exists('../usr/2-raw-html')) {
+    mkdir('../usr/2-raw-html', 0777, true);
+}
+file_put_contents("../usr/2-raw-html/$page.html", $ret, FILE_USE_INCLUDE_PATH); 
 
 echo " - " . date("H:i:s", time());
 echo "</li>";
